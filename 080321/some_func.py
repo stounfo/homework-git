1be47df5ccd7b535e061b325f5cbcaf4cc39c0a8
Oleg Levantin <levantin.oleg@gmail.com>
2021-03-10 17:40:52 +0300

    Add solution with decorator

--- 080321/some_func.py
+++ 080321/some_func.py
@@ -1,22 +1,32 @@
-def logging(func_name, result):
-    with open('/tmp/logfile', 'a') as file:
-        file.write('\n')
-        file.write(f'{func_name}: {result}')
+def logging(func):
+    def wrapper(*args):
+        result = func(*args)
+        with open('/tmp/logfile', 'a') as file:
+            file.write('\n')
+            file.write(f'{func.__name__}: {result}')
+
+        return result
+
+    return wrapper
 
 
+@logging
 def sum(a, b):
     res = a + b
-    logging(func_name='sum', result=res)
     return res
 
 
+@logging
 def power(a, b):
     res = a ** b
-    logging(func_name='power', result=res)
     return res
 
 
+@logging
 def multiplication(a, b):
     res = a * b
-    logging(func_name='multiplication', result=res)
     return res
+
+
+if __name__ == '__main__':
+    a = sum(1, 10)
