d683b46f50cd918bb168499c36390435c1ed3bb1
Oleg Levantin <levantin.oleg@gmail.com>
2021-03-10 15:36:37 +0300

    another solution

--- 080321/some_func.py
+++ 080321/some_func.py
@@ -1,25 +1,22 @@
-def sum(a, b):
-    res = a + b
+def logging(func_name, result):
     with open('/tmp/logfile', 'a') as file:
         file.write('\n')
-        file.write(f'sum: {res}')
+        file.write(f'{func_name}: {result}')
 
+
+def sum(a, b):
+    res = a + b
+    logging(func_name='sum', result=res)
     return res
 
 
 def power(a, b):
     res = a ** b
-    with open('/tmp/logfile', 'a') as file:
-        file.write('\n')
-        file.write(f'power: {res}')
-
+    logging(func_name='power', result=res)
     return res
 
 
 def multiplication(a, b):
     res = a * b
-    with open('/tmp/logfile', 'a') as file:
-        file.write('\n')
-        file.write(f'multiplication: {res}')
-
+    logging(func_name='multiplication', result=res)
     return res
